eefaf826dbb7fab4d86875ec240852491f7342cb
Rajeev Nayak <jeev.nayak@gmail.com>
2015-10-26 14:46:13 -0700

    initial move logic

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Fighter : MonoBehaviour {
+
+	private enum WeaponState {Empty, Rock, Paper, Scissors};
+
+	private WeaponState weaponState;
+
+	void Start () {
+		weaponState = WeaponState.Empty;
+	}
+	
+	public Move ExecuteMove (Move move) {
+		if (move.Equals(Move.Rock)) {
+			weaponState = WeaponState.Rock;
+		} else if (move.Equals(Move.Paper)) {
+			weaponState = WeaponState.Paper;
+		} else if (move.Equals(Move.Scissors)) {
+			weaponState = WeaponState.Scissors;
+		} else if (move.Equals (Move.Shoot)) {
+			weaponState = WeaponState.Empty;
+		}
+		return move;
+	}
+
+	public Move ExecuteRandomMove () {
+		return ExecuteMove (GenerateRandomValidMove ());
+	}
+
+	Move GenerateRandomValidMove () {
+		Move move = GetRandomMove();
+		while (move.Equals(Move.Shoot) && weaponState.Equals(WeaponState.Empty)) {
+			move = GetRandomMove();
+		}
+		return move;
+	}
+	
+	Move GetRandomMove () {
+		Array moves = Enum.GetValues(typeof(Move));
+		return (Move)moves.GetValue(UnityEngine.Random.Range(0, moves.Length));
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Move {Rock, Paper, Scissors, Shoot, Sidestep};
+
+public class GameController : MonoBehaviour {
+
+	public Fighter player;
+	public Fighter opponent;
+	public Text consoleText;
+
+	private int queueSize;
+	private List<Move> queuedMoves;
+
+	void Start () {
+		consoleText.text = "";
+
+		queueSize = 1;
+		queuedMoves = new List<Move>();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			queuedMoves.Add(Move.Rock);
+		} else if (Input.GetKeyDown(KeyCode.S)) {
+			queuedMoves.Add(Move.Paper);
+		} else if (Input.GetKeyDown(KeyCode.D)) {
+			queuedMoves.Add(Move.Scissors);
+		} else if (Input.GetKeyDown(KeyCode.F)) {
+			queuedMoves.Add(Move.Shoot);
+		} else if (Input.GetKeyDown(KeyCode.G)) {
+			queuedMoves.Add(Move.Sidestep);
+		}
+
+		if (queuedMoves.Count == queueSize) {
+			foreach (Move move in queuedMoves) {
+				Move playerMove = player.ExecuteMove(move);
+				consoleText.text += "You: " + playerMove.ToString() + "\n";
+
+				Move opponentMove = opponent.ExecuteRandomMove();
+				consoleText.text += "Opponent: " + opponentMove.ToString() + "\n";
+			}
+			queueSize = 2;
+			queuedMoves.Clear();
+		}
+	}
+}
