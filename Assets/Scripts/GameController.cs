ac94a053846a5d099047f8d8c5bf3b641c9e8c69
Rajeev Nayak <jeev.nayak@gmail.com>
2015-10-27 00:05:12 -0700

    initial implementation of multiplayer

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,116 +3,92 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public enum Weapon {Rock, Paper, Scissors};
-
-public class Move {
-
-	public enum MoveType {Load, Shoot, Sidestep};
-
-	public MoveType type;
-	public Weapon? weapon;
-
-	public Move (MoveType type, Weapon? weapon) {
-		this.type = type;
-		this.weapon = weapon;
-	}
-
-	public override string ToString ()
-	{
-		string ret = type.ToString();
-		if (weapon != null) {
-			ret += " " + weapon.ToString();
-		}
-		return ret;
-	}
-}
+public enum Move {Load, Shoot, Sidestep};
 
 public class GameController : MonoBehaviour {
 
-	public Fighter player;
-	public Fighter opponent;
 	public Text consoleText;
 	public Text hpText;
+	public int initQueueSize;
+	public int steadyStateQueueSize;
 
 	private int queueSize;
-	private List<Move> queuedMoves;
 
 	void Start () {
+		queueSize = initQueueSize;
+
 		consoleText.text = "";
-		UpdateHpText ();
-
-		queueSize = 1;
-		queuedMoves = new List<Move>();
+		hpText.text = "";
 	}
 
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.A)) {
-			queuedMoves.Add(new Move(Move.MoveType.Load, Weapon.Rock));
-		} else if (Input.GetKeyDown(KeyCode.S)) {
-			queuedMoves.Add(new Move(Move.MoveType.Load, Weapon.Paper));
-		} else if (Input.GetKeyDown(KeyCode.D)) {
-			queuedMoves.Add(new Move(Move.MoveType.Load, Weapon.Scissors));
-		} else if (Input.GetKeyDown(KeyCode.F)) {
-			queuedMoves.Add(new Move(Move.MoveType.Shoot, null));
-		} else if (Input.GetKeyDown(KeyCode.G)) {
-			queuedMoves.Add(new Move(Move.MoveType.Sidestep, null));
-		}
+	public int GetQueueSize () {
+		return queueSize;
+	}
 
-		if (queuedMoves.Count == queueSize) {
-			foreach (Move move in queuedMoves) {
-				if (move.type.Equals(Move.MoveType.Shoot)) {
-					move.weapon = player.GetLoadedWeapon();
-				}
-				Move playerMove = player.ExecuteMove(move);
-				consoleText.text += "You: " + playerMove.ToString() + "\n";
-
-				Move opponentMove = opponent.ExecuteRandomMove();
-				consoleText.text += "Opponent: " + opponentMove.ToString() + "\n";
-
-				ResolveMoves(playerMove, opponentMove);
+	public void OnWaitingForOthersChanged () {
+		PlayerController localPlayerController = null;
+		PlayerController remotePlayerController = null;
+		
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		foreach (GameObject player in players) {
+			PlayerController playerController = player.GetComponent<PlayerController>();
+			if (playerController.isLocalPlayer) {
+				localPlayerController = playerController;
+			} else {
+				remotePlayerController = playerController;
 			}
-			queueSize = 2;
-			queuedMoves.Clear();
+		}
+		
+		if (localPlayerController == null || remotePlayerController == null) {
+			return;
+		}
+
+		if (localPlayerController.GetWaitingForOthers() &&
+		    remotePlayerController.GetWaitingForOthers()) {
+			localPlayerController.CmdStartNextMove();
+			queueSize = steadyStateQueueSize;
 		}
 	}
 
-	void ResolveMoves (Move playerMove, Move opponentMove) {
-		if (playerMove.type.Equals (Move.MoveType.Shoot)) {
-			if (opponentMove.type.Equals(Move.MoveType.Load)) {
-				opponent.TakeHit();
-			} else if (opponentMove.type.Equals(Move.MoveType.Shoot)) {
-				if (WeaponBeatsOther(playerMove.weapon, opponentMove.weapon)) {
-					opponent.TakeHit();
-				}
+	public void OnQueuedMovesChanged () {
+		PlayerController localPlayerController = null;
+		PlayerController remotePlayerController = null;
+
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		foreach (GameObject player in players) {
+			PlayerController playerController = player.GetComponent<PlayerController>();
+			if (playerController.isLocalPlayer) {
+				localPlayerController = playerController;
+			} else {
+				remotePlayerController = playerController;
 			}
 		}
 
-		if (opponentMove.type.Equals (Move.MoveType.Shoot)) {
-			if (playerMove.type.Equals(Move.MoveType.Load)) {
-				player.TakeHit();
-			} else if (playerMove.type.Equals(Move.MoveType.Shoot)) {
-				if (WeaponBeatsOther(opponentMove.weapon, playerMove.weapon)) {
-					player.TakeHit();
-				}
-			}
+		if (localPlayerController == null || remotePlayerController == null) {
+			return;
 		}
 
-		UpdateHpText ();
-	}
-
-	void UpdateHpText () {
-		hpText.text = "Your HP: " + player.hp + "\nOpponent's HP: " + opponent.hp;
-	}
-
-	bool WeaponBeatsOther (Weapon? weapon, Weapon? other) {
-		if (weapon.Equals (Weapon.Rock)) {
-			return other.Equals(Weapon.Scissors);
-		} else if (weapon.Equals (Weapon.Scissors)) {
-			return other.Equals(Weapon.Paper);
-		} else if (weapon.Equals (Weapon.Paper)) {
-			return other.Equals(Weapon.Rock);
+		if (!localPlayerController.IsQueueFull() || !remotePlayerController.IsQueueFull()) {
+			return;
 		}
 
-		return false;
+		for (int i = 0; i < queueSize; i++) {
+			ResolveMove(localPlayerController, remotePlayerController, i);
+		}
+
+		localPlayerController.CmdWaitForOthers();
+	}
+
+	void ResolveMove (PlayerController localPlayerController,
+	                  PlayerController remotePlayerController,
+	                  int moveIndex) {
+		localPlayerController.ExecuteQueuedMove(moveIndex, remotePlayerController);
+		remotePlayerController.ExecuteQueuedMove(moveIndex, localPlayerController);
+		UpdateHpText (localPlayerController, remotePlayerController);
+	}
+
+	void UpdateHpText (PlayerController localPlayerController,
+	                   PlayerController remotePlayerController) {
+		hpText.text = "Your HP: " + localPlayerController.hp + "\nOpponent's HP: " + remotePlayerController.hp;
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : NetworkBehaviour {
+
+	public int hp;
+
+	private GameController gameController;
+	private Text consoleText;
+	private bool loaded;
+
+	[SyncVar(hook="OnWaitingForOthersChanged")]
+	private bool waitingForOthers;
+
+	private SyncListInt queuedMoves = new SyncListInt();
+
+	void Start () {
+		gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+		consoleText = GameObject.Find("Console Text").GetComponent<Text>();
+		loaded = false;
+
+		queuedMoves.Callback += OnQueuedMovesChanged;
+	}
+
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		if (queuedMoves.Count == gameController.GetQueueSize()) {
+			return;
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			CmdAddQueuedMove(Move.Load);
+		} else if (Input.GetKeyDown(KeyCode.S)) {
+			CmdAddQueuedMove(Move.Shoot);
+		} else if (Input.GetKeyDown(KeyCode.D)) {
+			CmdAddQueuedMove(Move.Sidestep);
+		}
+	}
+
+	public bool GetWaitingForOthers () {
+		return waitingForOthers;
+	}
+
+	public bool IsQueueFull () {
+		return queuedMoves.Count == gameController.GetQueueSize();
+	}
+
+	[Command]
+	public void CmdWaitForOthers () {
+		waitingForOthers = true;
+	}
+	
+	void OnWaitingForOthersChanged (bool newWaitingForOthers) {
+		waitingForOthers = newWaitingForOthers;
+		gameController.OnWaitingForOthersChanged();
+	}
+
+	[Command]
+	void CmdAddQueuedMove (Move move) {
+		queuedMoves.Add((int)move);
+	}
+
+	[Command]
+	public void CmdStartNextMove () {
+		waitingForOthers = false;
+		queuedMoves.Clear();
+	}
+
+	void OnQueuedMovesChanged (SyncListInt.Operation op, int index) {
+		gameController.OnQueuedMovesChanged();
+	}
+
+	public void ExecuteQueuedMove (int moveIndex, PlayerController otherPlayerController) {
+		Move myMove = (Move)queuedMoves[moveIndex];
+		Move otherPlayerMove = (Move)otherPlayerController.queuedMoves[moveIndex];
+
+		string playerStr = isLocalPlayer ? "Your" : "Opponent's";
+		consoleText.text += playerStr + " Move: " + myMove.ToString() + "\n";
+		
+		if (myMove.Equals(Move.Load)) {
+			loaded = true;
+		} else if (myMove.Equals(Move.Shoot) && loaded) {
+			if (otherPlayerMove.Equals(Move.Load)) {
+				otherPlayerController.TakeHit();
+			}
+			loaded = false;
+		}
+	}
+
+	public void TakeHit () {
+		hp--;
+	}
+}
