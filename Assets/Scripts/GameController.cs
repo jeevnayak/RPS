54d88420d7e281fae84baf1164ba9ac58c84ea73
Sean Liu <sean.y.liu@gmail.com>
2015-10-27 15:11:13 -0700

    made the ship part of the player prefab vs respawning it

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,13 +12,10 @@ public class GameController : MonoBehaviour {
 	public Text hpText;
 	public int initQueueSize;
 	public int steadyStateQueueSize;
-	public GameObject spaceship;
 	public GameObject player;
 
 	private PlayerController localPlayer = null;
-	private GameObject localPlayerShip = null;
 	private PlayerController remotePlayer = null;
-	private GameObject remotePlayerShip = null;
 	private int queueSize;
 	private bool singlePlayer;
 
@@ -34,7 +31,7 @@ public class GameController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.P) && remotePlayer == null) {
 			singlePlayer = true;
 			GameObject.Find("Network Manager").GetComponent<NetworkManager>().StartHost();
-			Instantiate(player, new Vector3 (0, 0, 0), Quaternion.identity);
+			Instantiate(player, new Vector3 (0, 1, 3), Quaternion.identity);
 		}
 	}
 
@@ -44,21 +41,12 @@ public class GameController : MonoBehaviour {
 
 	public void SetLocalPlayer (PlayerController player) {
 		localPlayer = player;
-
-		if (localPlayerShip == null) {
-			localPlayerShip = Instantiate(spaceship, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
-		}
 	}
 	
 	public void SetRemotePlayer (PlayerController player) {
 		remotePlayer = player;
 		if (singlePlayer) {
 			remotePlayer.MakeAutomated();
-		}
-
-		if (remotePlayerShip == null) {
-			Vector3 spawnPosition = new Vector3 (0, 1, 3);
-			remotePlayerShip = Instantiate(spaceship, spawnPosition, Quaternion.Euler(0, 180, 0)) as GameObject;
 		}
 	}
 
