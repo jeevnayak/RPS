6e2d4c686a6ad05e10579c36d0f94b53f947a1e1
Sean Liu <sean.y.liu@gmail.com>
2015-10-28 15:07:28 -0700

    background movement

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,15 +2,27 @@
 using System.Collections;
 
 public class Rotate : MonoBehaviour {
+	private float maxDistance = 60.0f;
+	private float starDistance = 30.0f;
+
+	private float maxDistanceSqr;
+	private Vector3 originalPosition;
 
 	// Use this for initialization
 	void Start () {
-	
+		maxDistanceSqr = maxDistance * maxDistance;
+		originalPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(Vector3.left * Time.deltaTime * 2);
 		transform.Rotate(Vector3.up * Time.deltaTime * 2, Space.World);
+
+		transform.Translate(Vector3.back * Time.deltaTime * 1, Space.World);
+
+		if ((transform.position).sqrMagnitude > maxDistanceSqr) {
+			transform.position = originalPosition + Vector3.forward * 20;
+		}
 	}
 }
